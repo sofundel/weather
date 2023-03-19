1a53b9e73b6e5802337c2f316a785ae8c18af123
sofundel <sofundel@yandex.ru>
2023-03-19 11:56:14 +0300

    Переименован файл html

--- constants-server.js
+++ constants-server.js
@@ -1,7 +1,7 @@
 export { SERVER }
 
 const SERVER = {
-    SERVER_URL: 'http://api.openweathermap.org/data/2.5/weather',
-    FORECAST_URL : 'http://api.openweathermap.org/data/2.5/forecast',
+    SERVER_URL: 'https://api.openweathermap.org/data/2.5/weather',
+    FORECAST_URL : 'https://api.openweathermap.org/data/2.5/forecast',
     API_KEY: '9a4679d74d6b2705dcf83c96c7e6b1f9',
 }
